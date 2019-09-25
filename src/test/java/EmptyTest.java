ea87d04148fdd7bd58bef1e83fea8759c6f245fb
Ilya Zinchuk <ilya.zinchuk@happn.fr>
2019-09-25 17:28:08 +0200

    initial commit

---
+++ src/test/java/EmptyTest.java
@@ -0,0 +1,10 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class EmptyTest {
+    @Test
+    public void dummyTest()
+    {
+        Assertions.fail("You'd better start writing code!");
+    }
+}
